cd8aa806d00cc6f57f3fe499deb113217b0c2e87
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:31 +0530

    Add files via upload
    MY FIRST PYTHON COMMIT

---
+++ student_details.py
@@ -0,0 +1,7 @@
+name = input('Please enter your name:')
+print('Name of the user is:','\n',name)
+roll_number = int(input('please enter your roll number:'))
+print('your roll number is:','\n',roll_number)
+cgpa= float(input('please enter your cgpa'))
+percentage= cgpa*10
+print('percentage obtained=',percentage)
