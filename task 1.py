5c6f189a569b8f9da3aa87d1647899999b6f7c3d
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-08 13:38:17 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,8 @@
+'''
+Author: Abhijith P
+Date:07-10-2024
+Python program that asks the user their name and age then display their name and age as output
+'''
+name=input("Please enter your name:")
+age=int(input("Please enter your age:"))
+print("hello",name,'you are',age,'years old')
