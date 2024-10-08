98111e0fdaed5407862fed286ee41569b0f49f63
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-08 14:04:29 +0530

    Add files via upload

---
+++ BASIC OPERATIONS.py
@@ -0,0 +1,20 @@
+'''
+AUTHOR:ABHIJITH P
+DATE:08/10/2024
+Python program that does some basic operations and an algebraic expression
+'''
+number1=int(input('PLEASE ENTER THE NUMBER A:'))
+number2=int(input('PLEASE ENTER THE NUMBER B'))
+number3=int(input('PLEASE ENTER THE NUMBER C'))
+SUM=number1+number2
+print('The sum of number1 and number2 is:',SUM)
+SUBTRACTION=number1-number2
+print('The difference when number2 is subtracted from number2 is:',SUBTRACTION)
+MULTIPLICATION=number1*number2
+DIVISION=number1/number2
+REMINDER=number1%number2
+ARITHEMATIC=(number1+number2)*number3/2
+print('The result of number1 multiplied by number2 is:',MULTIPLICATION)
+print('The result of number1 divided by number2 is:',DIVISION)
+print('The reminder when number1 is divided by number2 is:',REMINDER)
+print('the result of (number1+number2)*number3/2 is:',ARITHEMATIC)
---
+++ concatenated string.py
@@ -0,0 +1,8 @@
+'''
+AUTHOR:ABHIJITH P
+DATE:08/10/2024
+Python program that concatenates the users first and last name the print the final string
+'''
+string1="ABHIJITH"
+string2='P'
+print(string1+' '+string2)
