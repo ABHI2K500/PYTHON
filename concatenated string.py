2071c5ea3baa0c8bdebb67b825a5af935e5d8d2c
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-08 14:57:41 +0530

    Update concatenated string.py

--- concatenated string.py
+++ concatenated string.py
@@ -3,6 +3,14 @@ AUTHOR:ABHIJITH P
 DATE:08/10/2024
 Python program that concatenates the users first and last name the print the final string
 '''
-string1="ABHIJITH"
+string1="ABHIJITH "
 string2='P'
-print(string1+' '+string2)
+string3=string1+string2
+print(len(string3))
+print(string3)
+length=len(string3)
+string4=string3[length:]
+firstname=string3[:length-2]
+print(string4)
+print(string1+string2)
+print(firstname)
