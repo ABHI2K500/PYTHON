50e3ce9efdce4159fd61a06a89bc937a884f3c2c
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-15 14:20:20 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,9 @@
+'''
+Author: Abhijith P
+Date:15-10-2024
+to calculate the simple interest'''
+principle=float(input("enter the priciple value:"))
+rate=float(input("enter the rate:"))
+time=float(input("enter the number of years"))
+simple_interest=(principle*time*rate)/100
+print("the simple interest is:",simple_interest)
