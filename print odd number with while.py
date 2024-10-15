9cdd586175701910915b05a21e260b41078765e7
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-15 16:10:03 +0530

    Add files via upload
    excluding the limit

--- print odd number with while.py
+++ print odd number with while.py
@@ -1,12 +1,12 @@
 '''
 Author: Abhijith P
 Date:15-10-2024
-python program to display n number of odd numbers
+python program to display odd numbers with limit
 '''
 num=int(input("how many off numbers do u need"))
 odd=1
 a=0
-while a<=num:
-    print(odd,"\t",end="")
+while a<num:
+    print(odd,end="  ")
     odd+=2
     a+=1
