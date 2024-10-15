d954964963c6667b55bc6fe7986f4b72bf6fbbd2
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-15 14:23:17 +0530

    Add files via upload

---
+++ largest of two numbers.py
@@ -0,0 +1,13 @@
+'''
+Author: Abhijith P
+Date:15-10-2024
+to check the greatest of two numbers
+'''
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+if num1>num2:
+    print("number 1 is the greatest")
+elif num1==num2:
+    print("both numbers are equal")
+else:
+    print("number 2 is greater")
