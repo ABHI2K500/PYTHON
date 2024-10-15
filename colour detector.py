c6b8744f2ec830af3903fc7971ed2dc399bb932a
ABHI2K500 <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-15 14:34:43 +0530

    Add files via upload

---
+++ colour detector.py
@@ -0,0 +1,14 @@
+'''
+Author: Abhijith P
+Date:15-10-2024
+to give a message based on the given color code
+'''
+code=input("Enter any one of the following code R/G/B")
+if code=='R' or code=='r':
+    print("Red")
+elif code=='G' or code=='g':
+    print("Green")
+elif code=='B' or code=='b':
+    print("Blue")
+else:
+    print("wrong code")
