c71a9effe4e12fdf6f869254b17fdad78bfcf778
ABHIJITH P <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-29 16:18:34 +0530

    Update vowels in a string.py

--- vowels in a string.py
+++ vowels in a string.py
@@ -3,8 +3,11 @@ Author:Abhijith P
 Date: 29/10/2024
 python program to print the vowels in an string
 '''
-_string=input("Enter the string:")
+input_string=input("Enter the string:")
 vowels=('a','e','i','o','u','A','E','I','O','U')
-for i in _string:
+vowel_count
+for i in input_string:
     if i in vowels:
+        vowel_count+=1
         print(i,end=" ")
+print(vowel_count)
