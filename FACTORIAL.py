fc5c02a62b7ef8f878fb83d98992c098528911a1
ABHIJITH P <abhijithp2028@cs.sjcetpalai.ac.in>
2024-10-29 16:23:19 +0530

    Update FACTORIAL.py

--- FACTORIAL.py
+++ FACTORIAL.py
@@ -1,3 +1,8 @@
+'''
+Author:Abhijith P
+Date: 29/10/2024
+python program to dipsplay factorial of a number using for loop
+'''
 number=int(input("enter a number:"))
 factorial=1
 for i in range(1,number+1):
